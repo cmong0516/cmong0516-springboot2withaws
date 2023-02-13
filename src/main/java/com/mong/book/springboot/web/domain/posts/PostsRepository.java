7c99a15edb2735dc5c53ede7246e4390e1e85fbf
cmong0516 <cmong0516@gmail.com>
2023-02-13 14:34:19 +0900

    2023.02.13

--- src/main/java/com/mong/book/springboot/web/domain/posts/PostsRepository.java
+++ src/main/java/com/mong/book/springboot/web/domain/posts/PostsRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostsRepository extends JpaRepository<Posts,Long> {
     @Query("select p from Posts p ORDER BY p.id DESC")
     List<Posts> findAllDesc();
--- src/main/java/com/mong/book/springboot/web/domain/user/UserRepository.java
+++ src/main/java/com/mong/book/springboot/web/domain/user/UserRepository.java
@@ -3,7 +3,9 @@ package com.mong.book.springboot.web.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
 }
